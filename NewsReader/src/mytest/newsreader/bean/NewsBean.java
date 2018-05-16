560e47ed3f23b0c3b991ad105bf4992360f5a5dc
悟空别走 <tfcpanda@outlook.com>
2018-05-16 17:32:42 +0800

    添加一个新类
    用来描述新闻

---
+++ NewsReader/src/mytest/newsreader/bean/NewsBean.java
@@ -0,0 +1,10 @@
+package mytest.newsreader.bean;
+
+public class NewsBean {
+	public String title;
+	public String link;
+	public String description;
+	public String pubDate;
+	public String guid;
+	
+}
